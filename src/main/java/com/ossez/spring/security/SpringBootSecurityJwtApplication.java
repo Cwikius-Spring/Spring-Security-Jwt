fe062a709aef354604e5a2e3b23eaca15fad58ee
YuCheng Hu <huyuchengus@gmail.com>
2022-10-03 00:12:00 -0400

    更新 Spring 项目启动程序

--- src/main/java/com/ossez/spring/security/SpringBootSecurityJwtApplication.java
+++ src/main/java/com/ossez/spring/security/SpringBootSecurityJwtApplication.java
@@ -1,13 +1,36 @@
 package com.ossez.spring.security;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
+/**
+ * SPRING APPLICATION
+ *
+ * @author YuCheng Hu
+ */
 @SpringBootApplication
-public class SpringBootSecurityJwtApplication {
+@Slf4j
+public class SpringBootSecurityJwtApplication implements ApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-    SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
-	}
+    @Override
+    public void run(ApplicationArguments args) {
+        log.info("Application started with command-line arguments: {}", Arrays.toString(args.getSourceArgs()));
+        log.info("NonOptionArgs: {}", args.getNonOptionArgs());
+        log.info("OptionNames: {}", args.getOptionNames());
 
+        for (String name : args.getOptionNames()) {
+            log.info("arg-" + name + "=" + args.getOptionValues(name));
+        }
+
+        boolean containsOption = args.containsOption("person.name");
+        log.info("Contains person.name: " + containsOption);
+    }
 }
